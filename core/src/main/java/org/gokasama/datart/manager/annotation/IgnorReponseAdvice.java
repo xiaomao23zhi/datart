84761e0e5368e3143d9204640ce6ee6a89821edf
wujia <wujia@chinamobile.com>
2020-09-17 15:41:37 +0800

    cleanup

--- core/src/main/java/org/gokasama/datart/manager/annotation/IgnorReponseAdvice.java
+++ core/src/main/java/org/gokasama/datart/manager/annotation/IgnorReponseAdvice.java
@@ -1,14 +1,12 @@
 package org.gokasama.datart.manager.annotation;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 /**
  * @author ka wujia@chinamobile.com
  */
 @Target({ElementType.TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
+@Documented
 public @interface IgnorReponseAdvice {
 }
