c72cbbea83678b75be63ea660648e68a17bf2648
wujia <wujia@chinamobile.com>
2020-09-17 14:52:05 +0800

    add task executor

--- manager/src/main/java/org/gokasama/datart/manager/ManagerApplication.java
+++ manager/src/main/java/org/gokasama/datart/manager/ManagerApplication.java
@@ -2,13 +2,15 @@ package org.gokasama.datart.manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import springfox.documentation.oas.annotations.EnableOpenApi;
 
 /**
  * @author ka wujia@chinamobile.com
  */
-@EnableOpenApi
 @SpringBootApplication
+@EnableOpenApi
+@EnableAsync
 public class ManagerApplication {
 
     public static void main(String[] args) {
