3f258939b23905137fbbc37921d116ca5cfd203a
wujia <wujia@chinamobile.com>
2020-09-17 17:49:37 +0800

    fix RedisLockAspect

--- core/src/main/java/org/gokasama/datart/core/aop/RedisLockAspect.java
+++ core/src/main/java/org/gokasama/datart/core/aop/RedisLockAspect.java
@@ -16,7 +16,6 @@ import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 import java.lang.reflect.Method;
-import java.util.concurrent.TimeUnit;
 
 /**
  * @author ka wujia@chinamobile.com
@@ -27,10 +26,9 @@ import java.util.concurrent.TimeUnit;
 @Slf4j
 public class RedisLockAspect {
 
+    private static final String LOCK_KEY_PREFIX = "redisLock_";
     @Resource
     private Redisson redisson;
-
-    private static final String LOCK_KEY_PREFIX = "redisLock-";
 
     @Pointcut("@annotation(org.gokasama.datart.core.annotation.RedisLock)")
     public void redisLockPointcut() {
@@ -40,51 +38,63 @@ public class RedisLockAspect {
     @Around("redisLockPointcut()")
     public Object around(ProceedingJoinPoint proceedingJoinPoint) {
 
-        Object object = null;
-
+        Object object;
 
         MethodSignature methodSignature = (MethodSignature) proceedingJoinPoint.getSignature();
         Method method = methodSignature.getMethod();
 
         RedisLock annotation = method.getAnnotation(RedisLock.class);
+        String name;
+        String key;
         RLock rLock = null;
 
         try {
             if (annotation == null) {
                 //No annotation
-                log.info("<RedisLock> - No annotation");
+                log.info("No annotation");
                 object = proceedingJoinPoint.proceed();
                 return object;
             }
 
-            //Get lock
-            log.info("<RedisLock> - getLock with name:{}, key:{}, waitTime:{}, leaseTime:{}",
-                    annotation.name(), annotation.key(), annotation.waitTime(), annotation.leaseTime());
-            rLock = redisson.getLock(annotation.key());
+            name = annotation.name();
+            key = LOCK_KEY_PREFIX + annotation.key();
 
-            //Try lock
-            boolean isLock = rLock.tryLock(annotation.waitTime(), annotation.leaseTime(), annotation.timeUnit());
-            //If failed then return null(no proceed)
-            if (!isLock) {
-                log.info("<RedisLock> - Failed tryLock, name:{}, key:{}, waitTime:{}, leaseTime:{}",
-                        annotation.name(), annotation.key(), annotation.waitTime(), annotation.leaseTime());
-                return null;
+            //Get lock
+            log.info("GetLock, name:{}, key:{}, waitTime:{}, leaseTime:{}",
+                    name, key, annotation.waitTime(), annotation.leaseTime());
+            rLock = redisson.getLock(key);
+
+            try {
+                //Try lock
+                boolean isLock = rLock.tryLock(annotation.waitTime(), annotation.leaseTime(), annotation.timeUnit());
+                //If failed then return null(no proceed)
+                if (!isLock) {
+                    log.info("Failed tryLock, name:{}, key:{}, waitTime:{}, leaseTime:{}",
+                            name, key, annotation.waitTime(), annotation.leaseTime());
+                    return null;
+                }
+
+                //proceed
+                object = proceedingJoinPoint.proceed();
+                log.info("Success, name:{}, key:{}, waitTime:{}, leaseTime:{}",
+                        name, key, annotation.waitTime(), annotation.leaseTime());
+
+            } catch (Exception exception) {
+                //Error
+                log.error("Error", exception);
+                throw new RedisLockException("<RedisLock> - error: " + exception.getMessage());
+            } finally {
+                //Release
+                log.info("Release, name:{}, key:{}, waitTime:{}, leaseTime:{}",
+                        name, key, annotation.waitTime(), annotation.leaseTime());
+                if (rLock != null) {
+                    rLock.unlock();
+                }
             }
-            object = proceedingJoinPoint.proceed();
-            log.info("<RedisLock> - Success, name:{}, key:{}, waitTime:{}, leaseTime:{}",
-                    annotation.name(), annotation.key(), annotation.waitTime(), annotation.leaseTime());
 
         } catch (Throwable throwable) {
-            log.error("<RedisLock> - error", throwable);
-            throw new RedisLockException("<RedisLock> - error: " + throwable.getMessage());
-        } finally {
-            //Release
-            log.info("<RedisLock> - Release, name:{}, key:{}, waitTime:{}, leaseTime:{}",
-                    annotation.name(), annotation.key(), annotation.waitTime(), annotation.leaseTime());
-            if (rLock != null) {
-                rLock.unlock();
-
-            }
+            log.error("Error", throwable);
+            throw new RedisLockException("Error: " + throwable.getMessage());
         }
 
         return object;
