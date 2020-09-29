3161d547d8abe881076888287247efbcfcddc428
wujia <wujia@chinamobile.com>
2020-09-29 16:24:55 +0800

    update

--- docker/docker_run_neo4j.sh
+++ docker/docker_run_neo4j.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run -d --name neo4j -p 7474:7474 -p 7687:7687 --env=NEO4J_AUTH=none -v C:\Users\cmcc\Documents\neo4j:/var/lib/neo4j/import neo4j
+docker run -d --name neo4j -p 7474:7474 -p 7687:7687 --env=NEO4J_AUTH=none -v C:\Users\cmcc\Documents\data\neo4j:/var/lib/neo4j/import neo4j
